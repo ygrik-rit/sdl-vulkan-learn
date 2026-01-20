dd78df93d574b07a51f9dbf0ed19bafa1194146c
Stepanov Igor <i.stepanov@rit.va>
2026-01-20 17:41:10 +0300

    rework structure

--- cmake/options-config.cmake
+++ cmake/options-config.cmake
@@ -1,5 +1,26 @@
+include(CMakeDependentOption)
+
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    set(is_linux ON)
+endif()
+
 option(
     SVL_DEBUG_3RDPARTY
     "Force build 3rdparty libs localy for comfortable debugging"
+    OFF
+)
+
+cmake_dependent_option(
+    SVL_WAYLAND
+    "Built with support Wayland"
+    ON
+    is_linux
+    OFF
+)
+cmake_dependent_option(
+    SVL_X11
+    "Built with support X11"
+    OFF
+    is_linux
     OFF
 )
