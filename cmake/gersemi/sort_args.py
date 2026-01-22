c3290226739409891666cbf4ed5d3a1680205698
Stepanov Igor <i.stepanov@rit.va>
2026-01-22 12:15:12 +0300

    sonar cube fixes

--- cmake/gersemi/sort_args.py
+++ cmake/gersemi/sort_args.py
@@ -3,15 +3,14 @@
 from gersemi.builtin_commands import builtin_commands
 
 modified_target_sources = builtin_commands["target_sources"]
-modified_target_sources["keyword_preprocessors"] = {
-    key: "sort+unique" for key in modified_target_sources["multi_value_keywords"]
-}
+modified_target_sources["keyword_preprocessors"] = dict.fromkeys(
+    modified_target_sources["multi_value_keywords"], "sort+unique"
+)
 
 modified_target_link_libraries = builtin_commands["target_link_libraries"]
-modified_target_link_libraries["keyword_preprocessors"] = {
-    key: "sort+unique" for key in modified_target_link_libraries["multi_value_keywords"]
-}
-
+modified_target_link_libraries["keyword_preprocessors"] = dict.fromkeys(
+    modified_target_link_libraries["multi_value_keywords"], "sort+unique"
+)
 
 command_definitions = {
     "target_sources": modified_target_sources,
