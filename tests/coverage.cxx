1016fa04fc4b23008b63c5b726b9d29695f44559
Stepanov Igor <i.stepanov@rit.va>
2026-01-27 09:51:51 +0300

    try up procente of coverage

--- tests/coverage.cxx
+++ tests/coverage.cxx
@@ -7,6 +7,14 @@ import std;
 
 int main()
 {
+    try
+    {
+        svl::platform::window::context ctx("Coverage Test", 300, 400, {});
+    }
+    catch (svl::platform::window::window_error err)
+    {
+        spdlog::error("Global exception window_error: {}", err.what());
+    }
 
     return EXIT_SUCCESS;
 }
