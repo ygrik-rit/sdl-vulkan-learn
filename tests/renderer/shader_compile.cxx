1a9da2c5dfd1c45f8902d0ae6effa949be6ec1ec
Stepanov Igor <i.stepanov@rit.va>
2026-01-29 10:30:59 +0300

    setup null case for testing renderer shader

---
+++ tests/renderer/shader_compile.cxx
@@ -0,0 +1,10 @@
+#include <catch2/catch_test_macros.hpp>
+#include <libassert/assert-catch2.hpp>
+#include <spdlog/spdlog.h>
+
+#include "svl_config.hxx"
+
+import svl;
+import std;
+
+TEST_CASE("Compile shader", "[renderer][shader]") {}
