a332f1839d4246e125f9a39cca3f104f42388840
Stepanov Igor <ygrik2003@yandex.ru>
2026-04-21 12:22:48 +0300

    improve git submodule pipeline

--- cmake/functions-config.cmake
+++ cmake/functions-config.cmake
@@ -46,3 +46,49 @@ function(svl_add_resources)
         DESTINATION $<TARGET_FILE_DIR:${ARG_TARGET}>/${ARG_DESTINATION}
     )
 endfunction()
+
+function(svl_check_git_submodules)
+    find_package(Git)
+    if(NOT ${Git_FOUND})
+        return()
+    endif()
+
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} submodule status
+        OUTPUT_VARIABLE status_output
+        ERROR_QUIET
+        RESULT_VARIABLE result
+    )
+    execute_process(
+        COMMAND
+            ${GIT_EXECUTABLE} submodule foreach --quiet
+            "${GIT_EXECUTABLE} diff --quiet"
+        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+        TIMEOUT 5
+        RESULT_VARIABLE result_git_submodule_diff
+        OUTPUT_QUIET
+        COMMAND_ERROR_IS_FATAL NONE
+    )
+    if(result_git_submodule_diff EQUAL 128 OR status_output MATCHES "^-")
+        message(
+            WARNING
+            "Need submodule update. Running:\n"
+            "${GIT_EXECUTABLE} submodule foreach --quiet \"${GIT_EXECUTABLE} reset --hard\"\n"
+            "${GIT_EXECUTABLE} submodule update --init --recursive"
+        )
+        execute_process(
+            COMMAND
+                ${GIT_EXECUTABLE} submodule foreach --quiet
+                "${GIT_EXECUTABLE} reset --hard"
+            WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+            WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+    else()
+        message(STATUS "All submodules checkouted")
+    endif()
+endfunction()
